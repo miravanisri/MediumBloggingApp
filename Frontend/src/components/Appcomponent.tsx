d6ec78ecdd242c82af1123d23a39fc048384c513
Miravani sri <miravanisri123@gmail.com>
2024-10-11 20:43:02 +0530

    modified

--- Frontend/src/components/Appcomponent.tsx
+++ Frontend/src/components/Appcomponent.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom"
 import { Avatar } from "./Blogcard"
-import {jwt} from "jsonwebtoken"
-import { useEffect, useState } from "react"
+
 
 
 
@@ -19,7 +18,7 @@ return(
         <button type="button" className=" mr-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 text-center ">New</button>
         </Link>
   
-    <Avatar authorName={"User"} size="big"/>
+    <Avatar authorName={"User"} size="big"/> 
 
     </div>
 
